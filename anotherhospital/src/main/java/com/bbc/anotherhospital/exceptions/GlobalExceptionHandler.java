6b5ba7f702598dc65f67950113ed530431893cfd
Michał Barnat <michalbarnat@gmail.com>
2023-11-01 18:52:55 +0100

    handle AppointmentNotFoundException added: ErrorMessage.java, GlobalExceptionHandler.java

---
+++ anotherhospital/src/main/java/com/bbc/anotherhospital/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.bbc.anotherhospital.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DoctorNotFoundException.class)
+    public ResponseEntity<ErrorMessage>  handleDoctorNotFoundException(DoctorNotFoundException ex, HttpServletRequest request) {
+        return new ResponseEntity<>(ErrorMessage.builder()
+                .dateTime(LocalDateTime.now())
+                .code(NOT_FOUND.value())
+                .status(NOT_FOUND.getReasonPhrase())
+                .message(ex.getMessage())
+                .uri(request.getRequestURI())
+                .method(request.getMethod())
+                .build(), NOT_FOUND);
+    }
+
+    @ExceptionHandler(AppointmentNotFoundException.class)
+    public ResponseEntity<ErrorMessage>  handleAppointmentNotFoundException(AppointmentNotFoundException ex, HttpServletRequest request) {
+        return new ResponseEntity<>(ErrorMessage.builder()
+                .dateTime(LocalDateTime.now())
+                .code(NOT_FOUND.value())
+                .status(NOT_FOUND.getReasonPhrase())
+                .message(ex.getMessage())
+                .uri(request.getRequestURI())
+                .method(request.getMethod())
+                .build(), NOT_FOUND);
+    }
+
+}

59aa5e3cd7d66b007804a39bd7a851af41469c57
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-16 17:51:58 +0100

    DoctorController - update 1

--- anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/controller/DoctorController.java
+++ anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/controller/DoctorController.java
@@ -1,22 +1,33 @@
 package com.bbc.anotherhospital.doctor.controller;
 
+import com.bbc.anotherhospital.doctor.commands.CreateDoctorCommand;
+import com.bbc.anotherhospital.doctor.handlers.CreateDoctorCommandHandler;
 import com.bbc.anotherhospital.doctor.handlers.FindDoctorQueryHandler;
 import com.bbc.anotherhospital.doctor.snapshot.DoctorSnapshot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
     private final FindDoctorQueryHandler findDoctorQueryHandler;
+    private final CreateDoctorCommandHandler createDoctorCommandHandler;
 
     @Autowired
-    public DoctorController(FindDoctorQueryHandler findDoctorQueryHandler) {
+    public DoctorController(FindDoctorQueryHandler findDoctorQueryHandler, CreateDoctorCommandHandler createDoctorCommandHandler) {
         this.findDoctorQueryHandler = findDoctorQueryHandler;
+        this.createDoctorCommandHandler = createDoctorCommandHandler;
+    }
+
+    @PostMapping
+    public ResponseEntity<DoctorSnapshot> save(@RequestBody CreateDoctorCommand command) {
+        DoctorSnapshot doctorSnapshot = createDoctorCommandHandler.handle(command);
+        return new ResponseEntity<>(doctorSnapshot, CREATED);
     }
 
     @GetMapping("/{id}")
@@ -24,4 +35,6 @@ public class DoctorController {
         DoctorSnapshot doctor = findDoctorQueryHandler.handle(id);
         return ResponseEntity.ok(doctor);
     }
+
+
 }

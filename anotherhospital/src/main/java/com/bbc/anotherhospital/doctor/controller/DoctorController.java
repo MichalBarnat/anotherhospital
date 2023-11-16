698c3d71d1d14b3940994cd837d6fe27cdda9f9a
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-16 18:16:26 +0100

    DoctorController update 2

--- anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/controller/DoctorController.java
+++ anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/controller/DoctorController.java
@@ -1,8 +1,9 @@
 package com.bbc.anotherhospital.doctor.controller;
 
 import com.bbc.anotherhospital.doctor.commands.CreateDoctorCommand;
-import com.bbc.anotherhospital.doctor.handlers.CreateDoctorCommandHandler;
-import com.bbc.anotherhospital.doctor.handlers.FindDoctorQueryHandler;
+import com.bbc.anotherhospital.doctor.commands.CreateDoctorPageCommand;
+import com.bbc.anotherhospital.doctor.commands.UpdateDoctorCommand;
+import com.bbc.anotherhospital.doctor.handlers.*;
 import com.bbc.anotherhospital.doctor.snapshot.DoctorSnapshot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,19 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class DoctorController {
     private final FindDoctorQueryHandler findDoctorQueryHandler;
     private final CreateDoctorCommandHandler createDoctorCommandHandler;
+    private final FindAllDoctorsQueryHandler findAllDoctorsQueryHandler;
+    private final DeleteDoctorByIdCommandHandler deleteDoctorByIdCommandHandler;
+    private final EditDoctorCommandHandler editDoctorCommandHandler;
+    private final EditDoctorPartiallyCommandHandler editDoctorPartiallyCommandHandler;
 
     @Autowired
-    public DoctorController(FindDoctorQueryHandler findDoctorQueryHandler, CreateDoctorCommandHandler createDoctorCommandHandler) {
+    public DoctorController(FindDoctorQueryHandler findDoctorQueryHandler,EditDoctorPartiallyCommandHandler editPartiallyCommandHandler ,EditDoctorCommandHandler editDoctorCommandHandler ,DeleteDoctorByIdCommandHandler deleteDoctorByIdCommandHandler ,FindAllDoctorsQueryHandler findAllDoctorsQueryHandler, CreateDoctorCommandHandler createDoctorCommandHandler) {
         this.findDoctorQueryHandler = findDoctorQueryHandler;
         this.createDoctorCommandHandler = createDoctorCommandHandler;
+        this.findAllDoctorsQueryHandler = findAllDoctorsQueryHandler;
+        this.deleteDoctorByIdCommandHandler = deleteDoctorByIdCommandHandler;
+        this.editDoctorCommandHandler = editDoctorCommandHandler;
+        this.editDoctorPartiallyCommandHandler = editPartiallyCommandHandler;
     }
 
     @PostMapping
@@ -36,5 +45,28 @@ public class DoctorController {
         return ResponseEntity.ok(doctor);
     }
 
+    @GetMapping
+    public ResponseEntity<List<DoctorSnapshot>> findAll(CreateDoctorPageCommand command){
+        List<DoctorSnapshot> doctors = findAllDoctorsQueryHandler.handle(command);
+        return ResponseEntity.ok(doctors);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        deleteDoctorByIdCommandHandler.handle(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DoctorSnapshot> editDoctor(@PathVariable Long id, @RequestBody UpdateDoctorCommand command){
+        DoctorSnapshot doctorSnapshot = editDoctorCommandHandler.handle(id, command);
+        return ResponseEntity.ok(doctorSnapshot);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<DoctorSnapshot> editPartially(@PathVariable Long id, @RequestBody UpdateDoctorCommand command){
+        DoctorSnapshot doctorSnapshot = editDoctorPartiallyCommandHandler.handler(id, command);
+        return ResponseEntity.ok(doctorSnapshot);
+    }
 
 }
--- anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/handlers/FindAllDoctorsQueryHandler.java
+++ anotherhospital/src/main/java/com/bbc/anotherhospital/doctor/handlers/FindAllDoctorsQueryHandler.java
@@ -1,6 +1,7 @@
 package com.bbc.anotherhospital.doctor.handlers;
 
 import com.bbc.anotherhospital.doctor.commands.CreateDoctorCommand;
+import com.bbc.anotherhospital.doctor.commands.CreateDoctorPageCommand;
 import com.bbc.anotherhospital.doctor.repository.DoctorRepository;
 import com.bbc.anotherhospital.doctor.snapshot.DoctorSnapshot;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 public interface FindAllDoctorsQueryHandler {
-    List<DoctorSnapshot> handle(CreateDoctorCommand command);
+    List<DoctorSnapshot> handle(CreateDoctorPageCommand command);
 }
 
 @Service
@@ -20,7 +21,7 @@ class FindAllDoctorsQueryHandlerImpl implements FindAllDoctorsQueryHandler {
     private final ModelMapper modelMapper;
 
     @Override
-    public List<DoctorSnapshot> handle(CreateDoctorCommand command) {
+    public List<DoctorSnapshot> handle(CreateDoctorPageCommand command) {
         return doctorRepository.findAll().stream()
                 .map(app -> modelMapper.map(app, DoctorSnapshot.class))
                 .toList();
